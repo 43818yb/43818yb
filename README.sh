64e7a7e16268d157c1c2c64b2cac50b46ec8648e
43818yb <141997186+43818yb@users.noreply.github.com>
2023-08-11 15:17:50 +0800

    Create README.sh

---
+++ README.sh
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @43818yb
+- 👀 I’m interested in foto
+- 🌱 I’m currently learning linux command
+- 💞️ I’m looking to collaborate on nah
+- 📫 How to reach me ...oh maybe not do that
+
+<!---
+43818yb/43818yb is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
